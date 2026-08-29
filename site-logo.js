1d9a87d92f74cf41e1f2ba4a507184631437978d
FootballTalk <442kvsm77w@privaterelay.appleid.com>
2026-08-29 10:49:01 +0100

    Refresh site navigation for member accounts

--- site-logo.js
+++ site-logo.js
@@ -8,7 +8,7 @@
   };
 
   loadScript('header-logo.js?v=20260827-sitewide-2','headerLogoShared');
-  loadScript('persistent-tabs.js?v=20260827-2','persistentTabsShared');
+  loadScript('persistent-tabs.js?v=20260829-members-1','persistentTabsShared');
   loadScript('stats-route.js?v=20260827-1','statsRouteShared');
   loadScript('deadline-day.js?v=20260827-1','deadlineDayShared');
   if(/quiz\.html$/i.test(location.pathname)) loadScript('quiz-scoreboard.js?v=20260829-1','quizScoreboardShared');
